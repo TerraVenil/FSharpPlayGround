a96d9028772454242a513ffa5eae3142055a962e
Alexey Borodenko <kosunix@gmail.com>
2017-03-06 16:53:54 +0200

    Initial commit.

---
+++ FSharpPlayGround/Module1.fs
@@ -0,0 +1,13 @@
+﻿namespace FSharpPlayGround
+
+module Module1 =
+    
+    open System
+
+    let createItem1 =
+        Console.WriteLine("Hello from method createItem1")
+
+    let createItem2 =
+        Console.WriteLine("Hello from method createItem2")
+        Seq.empty<string>
+
---
+++ FSharpPlayGround/Program.fs
@@ -0,0 +1,10 @@
+﻿module Program =
+
+    open System
+    open FSharpPlayGround.Module1
+
+    let main() =
+        let data = createItem2
+
+        Console.ReadKey() |> ignore
+    main()
